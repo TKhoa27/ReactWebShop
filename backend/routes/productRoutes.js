acaa2536568eb0211767ce650157cc563f5d0cbe
TKhoa <97323230+TKhoa27@users.noreply.github.com>
2023-04-07 16:07:00 +0700

    add middleware and fetching from database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import AsyncHandler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel.js'
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @desc    Public
+router.get(
+  '/',
+  AsyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+  })
+)
+
+// @desc    Fetch single products
+// @route   GET /api/products/:id
+// @desc    Public
+router.get(
+  '/:id',
+  AsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+)
+
+export default router
